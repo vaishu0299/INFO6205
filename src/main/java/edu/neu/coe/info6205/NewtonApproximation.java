f9770e0b57a5fb38f9fca940f41b8c523d09645e
vaishu0299 <challagulla.v@northeastern.edu>
2022-05-17 23:00:52 -0400

    Assignment 0

--- src/main/java/edu/neu/coe/info6205/NewtonApproximation.java
+++ src/main/java/edu/neu/coe/info6205/NewtonApproximation.java
@@ -6,16 +6,16 @@ package edu.neu.coe.info6205;
 
 class NewtonApproximation {
     public static void main(String[] args) {
-        // Newton's Approximation to solve cos(x) = x
+        // Newton's Approximation to solve sin(x) = x
         double x = 1.0;
         int left = 200;
         for (; left > 0; left--) {
-            final double y = Math.cos(x) - x;
+            final double y = Math.sin(x) - x;
             if (Math.abs(y) < 1E-7) {
-                System.out.println("the solution to cos(x)=x is: " + x);
+                System.out.println("the solution to sin(x)=x is: " + x);
                 System.exit(0);
             }
-            x = x + y / (Math.sin(x) + 1);
+            x = x - y / (Math.cos(x) - 1);
         }
     }
 }
